0efb5bb085a078ef344493f04d3b2fbadf19808f
Martin Quintana <martinquintanarana@gmail.com>
2022-11-04 09:35:19 +0100

    04/11/22

---
+++ Ejercicios/Eventos/1 Cuenta_clicks_v1/app/src/main/java/com/example/cuenta_clicks_v1/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.cuenta_clicks_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    //Atributo contador para que cuente la cantidad de veces que se pulse el boton
+    private int contador = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+
+        //asociar mi objeto java con la view declarada en el codigo XML de layout
+        textView = findViewById(R.id.contador);
+    } //end onCreate
+
+    //Metodo onClick (cuando pulsa el boton) va sumando de uno en uno al contador
+    public void onClickPulsa(View view) {
+        //todo: opereaciones asociadas al click del boton PULSA
+        //mensaje significativo en la textview final
+        contador++;
+        textView.setText("Has pulsado " + contador + " veces");
+    }
+
+    //metodo onClick (cuando pulsa el boton) restaura el contador a cero
+    public void onClickFinalizar(View view) {
+        //todo: opereaciones asociadas al click del boton FINALIZAR
+        //mensaje significativo en la textview final
+        contador = 0;
+        textView.setText("Has pulsado " + contador + " veces");
+    }
+
+}
--- Ejercicios/NavegadorLinearLayout/app/src/main/java/com/example/navegador_linear_layout/MainActivity.java
+++ Ejercicios/NavegadorLinearLayout/app/src/main/java/com/example/navegador_linear_layout/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.navegador_linear_layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //asociar mi objeto java con la view declarada en el codigo XML del layout
+
+        textView = findViewById(R.id.TextViewRespuesta);
+    } //end onCreate
+
+    public void onClickBtnSi(View view) {
+        //todo: operaciones asociadas al click del boton SI
+        //mensaje significativo en la textview final
+        textView.setText(R.string.Si);
+
     }
+
+    public void onClickBtn(View view) {
+        if (view.getId() == R.id.btnNo) {
+            textView.setText(R.string.No);
+        } else if (view.getId() == R.id.btnTalVez) {
+            textView.setText(R.string.Tal_Vez);
+        }
+    }
+
+    //creacion de clase interna para implementar el escuchador
+    private class Aux implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //gestionar pulsacion del btn1
+            textView.setText("Boton 1 pulsado");
+        }
+    }
+    //crear objeto de la clase auxiliar y asignarlo al botón 1
+    Aux escuchador1 = new Aux();
+    btn1.setOnClickListener(escuchador1);
 }
+
