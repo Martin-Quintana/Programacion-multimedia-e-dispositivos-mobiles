e607b046b1670d2b6befbbbd1477c88593aa9d2b
Martin Quintana <martinquintanarana@gmail.com>
2022-11-14 11:07:40 +0100

    14/11/22

---
+++ Ejercicios/Activitys/Ciclo De Vida/app/src/main/java/com/example/ciclodevida/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.ciclodevida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Ejecutando onCreate()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onCreate()");//Mensaje de log
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Ejecutando onStart()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onStart()");//Mensaje de log
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Ejecutando onStop()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onStop()");//Mensaje de log
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Ejecutando onDestroy()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onDestroy()");//Mensaje de log
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Ejecutando onResume()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onResume()");//Mensaje de log
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Ejecutando onPause()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onPause()");//Mensaje de log
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Ejecutando onRestart()", Toast.LENGTH_SHORT).show(); //Mensaje como Toas por pantalla
+        Log.i("ciclo","Ejecutando onRestart()");//Mensaje de log
+    }
+
+    //Accion del boton finalizar
+    public void onClickBtn(View view) {
+        finish();
+    }
+}
