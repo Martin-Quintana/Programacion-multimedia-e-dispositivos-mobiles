5f37cc60fb2c8584388ff965f4b2e73eed1a6d0f
Martin Quintana <martinquintanarana@gmail.com>
2022-11-25 10:49:21 +0100

    25/11/22

--- Ejercicios/Activitys/Ejercicio 8 - Fecha_Hora/app/src/main/java/com/example/fecha_hora/MainActivity.java
+++ Ejercicios/Activitys/Ejercicio 8 - Fecha_Hora/app/src/main/java/com/example/fecha_hora/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.fecha_hora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.tpHora);
+        datePicker = findViewById(R.id.dpFecha);
+    }//Fin de onCreate()
+
+    public void onClickBtn(View view) {
+        int dia = datePicker.getDayOfMonth();
+        int mes = datePicker.getMonth();
+        int ano = datePicker.getYear();
+
+        int hora = timePicker.getCurrentHour();
+        int minuto = timePicker.getCurrentMinute();
+
+        Toast.makeText(this, dia + "/" + (mes + 1) + "/" + ano
+               + "\n" + hora + ":" + minuto, Toast.LENGTH_SHORT).show();
     }
 }
+
+
+
