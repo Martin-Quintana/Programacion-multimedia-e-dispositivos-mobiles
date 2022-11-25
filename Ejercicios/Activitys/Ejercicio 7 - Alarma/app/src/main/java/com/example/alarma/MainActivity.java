d3318f0d3695c858b9926c19fbb584ac8e0ec8df
Martin Quintana <martinquintanarana@gmail.com>
2022-11-25 09:17:45 +0100

    25/11/22

--- Ejercicios/Activitys/Ejercicio 7 - Alarma/app/src/main/java/com/example/alarma/MainActivity.java
+++ Ejercicios/Activitys/Ejercicio 7 - Alarma/app/src/main/java/com/example/alarma/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.alarma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }//Fin de onCreate()
+
+    public void onClickBtn(View view) {
+        //Contruimos el intent de manera que se inicie la alarma
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        intent.putExtra(AlarmClock.EXTRA_MESSAGE, "Hora de levantarse!!!!!!!!!");
+        intent.putExtra(AlarmClock.EXTRA_HOUR, 8);
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, 7);
+        intent.putExtra(AlarmClock.EXTRA_RINGTONE, "Rooster Alarm");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
