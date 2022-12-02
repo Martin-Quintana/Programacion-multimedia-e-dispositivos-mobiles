90c5ab11206d9034d10de4f608ff0ae670c513af
Martin Quintana <martinquintanarana@gmail.com>
2022-12-02 09:11:33 +0100

    2/12/22

--- Ejercicios/Activitys/Ejercicio 11 - Fotografia/app/src/main/java/com/example/fotografia/MainActivity.java
+++ Ejercicios/Activitys/Ejercicio 11 - Fotografia/app/src/main/java/com/example/fotografia/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.fotografia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private TextView textView;
+    private ImageView imageView;
+    private String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.IVRecibida);
+
+
+    }//Fin onCreate()
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+    public void onClickCamara(View view) {
+        dispatchTakePictureIntent();
     }
 }
