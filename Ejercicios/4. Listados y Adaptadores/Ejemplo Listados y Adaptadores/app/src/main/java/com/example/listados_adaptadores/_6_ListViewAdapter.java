0c66d801329dfb7d68c1d525815427cd6478271c
Martin Quintana <martinquintanarana@gmail.com>
2023-01-13 09:06:09 +0100

    13/1/23

---
+++ Ejercicios/4. Listados y Adaptadores/Ejemplo Listados y Adaptadores/app/src/main/java/com/example/listados_adaptadores/_6_ListViewAdapter.java
@@ -0,0 +1,14 @@
+package com.example.listados_adaptadores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _6_ListViewAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_6_list_view_adapter);
+    }
+}
