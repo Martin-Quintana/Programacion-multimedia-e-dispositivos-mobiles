106763abe31647487de463478b1eed0a15cefb0e
Martin Quintana <martinquintanarana@gmail.com>
2023-02-15 13:18:51 +0100

    last 15/02/23

--- Ejercicios/Notificaciones al Usuario/Dialogos/app/src/main/java/com/example/dialogos/MainActivity.java
+++ Ejercicios/Notificaciones al Usuario/Dialogos/app/src/main/java/com/example/dialogos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dialogos;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,15 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+    }   //end onCreate
 
     public void onClickBtn (View v){
         switch (v.getId()){
             case R.id.btnMensaje:
-                //dialogo_con_mensaje();
+                dialogo_con_mensaje();
                 break;
             case R.id.btn1:
-                //dialogo_con_1_boton();
+                dialogo_con_1_boton();
                 break;
             case R.id.btn2:
                 //dialogo_con_2_botones();
@@ -31,5 +32,30 @@ public class MainActivity extends AppCompatActivity {
         }//end switch
     }// end onClickBtn
 
+    //metodo para crear un dialogo con un boton
+    private void dialogo_con_mensaje(){
+        //crea un objeto de la clase AlertDialog.Builder "ventana de dialogo"
+        AlertDialog.Builder ventana = new AlertDialog.Builder(this); //this es el contexto
+        ventana.setTitle("Atencion"); //titulo del dialogo
+        ventana.setIcon(android.R.drawable.ic_dialog_alert); //icono del dialogo
+        ventana.setMessage("Este es un mensaje de aviso"); //mensaje del dialogo
+        ventana.show(); //muestra el dialogo
+    }//end dialogo_con_mensaje
 
+    //metodo para crear un dialogo con un boton
+    private void dialogo_con_1_boton() {
+        AlertDialog.Builder ventana = new AlertDialog.Builder(this); //this es el contexto
+        ventana.setTitle("Atencion"); //titulo del dialogo
+        ventana.setIcon(android.R.drawable.ic_dialog_alert); //icono del dialogo
+        ventana.setMessage("Concede aviso"); //mensaje del dialogo
+        ventana.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //todo codigo a ejecutar cuando se pulse el boton
+                //codigo a ejecutar cuando se pulse el boton
+                dialog.cancel(); //cierra el dialogo
+            }
+        }); //boton del dialogo
+        ventana.show(); //muestra el dialogo
+    }
 }
