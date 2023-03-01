2e8a3d438e3f3c6955636e6256f78765b410b782
Martin Quintana <martinquintanarana@gmail.com>
2023-03-01 11:14:16 +0100

    1/03/23

--- Ejercicios/Notificaciones al Usuario/Notificaciones_Barra_Estado/app/src/main/java/com/example/notificaciones_barra_estado/MainActivity.java
+++ Ejercicios/Notificaciones al Usuario/Notificaciones_Barra_Estado/app/src/main/java/com/example/notificaciones_barra_estado/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.notificaciones_barra_estado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickBtnLanzarNotificacion(View view) {
-
+        //1. Creal la notificacion
+        Notification.Builder nb = new Notification.Builder(this);
+        //2. Personalizar
+        nb.setSmallIcon(android.R.drawable.star_on);
+        nb.setTicker("Atención");
+        nb.setContentTitle("Aviso de llamada");
+        nb.setContentText("Esto es el mensaje que queremos transmitir");
+        //Transformar el icono drawable en un bitmap
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), android.R.drawable.sym_call_incoming);
+        nb.setLargeIcon(largeIcon);
+        //3. Preparar la accion asociada a la notificacion
+        Intent intent = new Intent(this, SecondActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        //4. Lanzar el NotificationManager
     }
 }
